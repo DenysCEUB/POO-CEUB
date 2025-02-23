2762b0630cc649b30f3ea9ebe7a53864ba0a1beb
Prof. Me. Denys Alves da Silva <denys.silva@ceub.edu.br>
2025-02-23 14:45:32 -0300

    Add files via upload
    AVA1-001

---
+++ IMC/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Denys Alves da Silva", 105.6, 1.73);
+        pessoa.mostrarPessoa();
+    }
+}
